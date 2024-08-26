b48d1d325d22bfe7717b060108ecf3fffcd29464
zhuzhuxia <742491842@qq.com>
2024-08-26 14:52:24 +0800

    add router for path /

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from "./Home";
+
+const About = () => <h2>About Page</h2>;
+const Contact = () => <h2>Contact Page</h2>;
 
 function App() {
-  return (
-    <div className="App">
-      React App
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
